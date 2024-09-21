8e5010b9bb52630827d6805fd520aa4ec4d93542
Slavus54 <x.mirosfromsibir@gmail.com>
2024-09-21 22:33:40 +0700

    :zap: Added durations of videos and updated timer

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ import './style.css'
 import {Datus} from 'datus.js'
 
 const datus = new Datus()
+
+const cards = document.querySelectorAll('.card__item-container')
 const date = document.getElementById('date')
 
-date.textContent = datus.now('date')
+cards.forEach(el => {
+    el.addEventListener('mousemove', e => {
+        const card = e.target
+        let time = card.getAttribute('data-time')
+
+        if (time) {
+            time = Math.floor(datus.time(time, 'deconvert') / 6e1)
+            time += datus.time(datus.now('time'), 'deconvert')
+            time = datus.time(time)
+
+            date.textContent = `Watched up to ${time} | ${datus.now('date')}`        
+        }
+    })
+})
 
 console.log('Work')
